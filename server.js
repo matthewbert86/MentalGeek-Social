59c2d1e84cb47dd8e3646f82ac10e8f34a1e3017
Matt <matthewbertweb@gmail.com>
2020-08-24 13:18:05 -0400

    I am getting a referenceError for router.post() within users.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 // Bring in our Database
 const connectDB = require('./config/db');
+// bring in validator
+const { check, validationResult } = require('express-validator');
 
 // Initialize app variable with express
 const app = express();
@@ -9,14 +11,18 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+// This will allow us to get the data in req.body
+app.use(express.json({ extended: false }));
+
 // res.send will send data to the browser
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes so we can access them
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/auth', require('./routes/api/auth'));
 
 // Set up PORT
 const PORT = process.env.PORT || 5000;
