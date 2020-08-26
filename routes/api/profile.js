4b2ff030020b638b61ccbc8baac2b5286cee2915
Matt <matthewbertweb@gmail.com>
2020-08-26 10:59:58 -0400

    set up profile.js to get current user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,34 @@
 const express = require('express');
 // bring in express router
 const router = express.Router();
+// bring in auth middleware
+const auth = require('../../middleware/auth');
+// bring in profile model
+const Profile = require('../../models/Profile');
+// bring in user model
+const User = require('../../models/User');
 
-// @route    GET api/profile/me
+// @route    GET api/profile/me - this endpoint for individual user profile
 // @desc     Test route
-// @access   Public
-router.get('/', (req, res) => res.send('Profile route'));
+// @access   Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    // use Profile.findOne to find individual user
+    // we will use Populate to bring in the name and avatar of user, which are in User.js model
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+    // check to see if there is no profile
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: 'There is no profile for this user.' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
