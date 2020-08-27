3ee54394a3a1c640a13f70785b238a80bce3e8b2
Matt <matthewbertweb@gmail.com>
2020-08-27 16:14:23 -0400

    added user post route. Postman test successful.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,10 +3,50 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
+// bring in models
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route    POST api/posts
 // @desc     Create a post
 // @access   Private
-router.post('/', (req, res) => res.send('Posts route'));
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // variable for user
+      const user = await User.findById(req.user.id).select('-password');
+      // variable for new post
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      // save new post
+      const post = await newPost.save();
+      // get the post back after it has been added and saved
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
