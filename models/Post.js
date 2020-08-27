4da2d2710de13b6298528aa7269eafc3e151c518
Matt <matthewbertweb@gmail.com>
2020-08-27 13:42:23 -0400

    Created Post model

---
+++ models/Post.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+// create this so we dont have to use mongoose.schema in the code
+const Schema = mongoose.Schema;
+
+// connect posts to user
+const PostSchema = new Schema({
+  // the user can only delete their own post, and shows which user created it
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  Likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
