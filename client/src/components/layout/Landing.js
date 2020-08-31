cf7a6e7ccfc210e1dff7ce11190368882809412c
Matt <matthewbertweb@gmail.com>
2020-08-31 13:07:50 -0400

    Set up auth folder for Login and Register. Having an issue with white screen appearing above landing image

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -5,10 +5,9 @@ const Landing = () => {
     <section className='landing'>
       <div className='dark-overlay'>
         <div className='landing-inner'>
-          <h1 className='x-large'>Developer Connector</h1>
+          <h1 className='x-large'>Mental Geek Community</h1>
           <p className='lead'>
-            Create a developer profile/portfolio, share posts and get help from
-            other developers
+            Create a profile, and share your stories in short little posts.
           </p>
           <div className='buttons'>
             <a href='register.html' className='btn btn-primary'>
